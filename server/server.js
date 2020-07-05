e0994a28b074e707b15f2281eeb2cd4994e19472
Sandeep Pandey <sandeep.pandey@galaxyweblinks.in>
2020-07-05 23:15:18 +0530

    Changes to server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const { auth } = require('./middlewares/auth');
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-
+app.use(express.static('client/build'));
 
 // GET //
 app.get('/api/auth', auth, (req, res) => {
@@ -145,6 +145,12 @@ app.delete('/api/delete_book', (req, res) => {
     });
 });
 
+if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
+    app.get('/*', (req,res) => {
+        res.sendfile(path.resolve(__dirname, '../client', 'build','index.html'))
+    })
+}
 
 const port = process.env.port || 3001;
 app.listen(port, () => {
